55c14abdf7d20b3374bcc51f02ec6d99dd07e3d9
Anuradha Kumari <anuradha.kumari@CaptainMarvel.local>
2019-08-03 09:40:08 +0530

    changes

--- src/Examples/Example3/A.test.js
+++ src/Examples/Example3/A.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
 
 import A from './A';
 
-describe.skip('A component', () => {
+describe('A component', () => {
   it('should SHALLOW render correctly', () => {
     const wrapper = shallow(<A />);
-    expect(shallowToJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
   it('should MOUNT correctly', () => {
     const wrapper = mount(<A />);
-    expect(shallowToJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
     wrapper.unmount();
   });
 });
